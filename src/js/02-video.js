fa82947ec9459d652dc292e6f6e229eba729b643
romaniv2511 <romaniv2511@gmail.com>
2022-09-22 20:19:55 +0300

    add Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import Storage from './storage'
+
+const LOCAL_STORAGE_KEY = "videoplayer-current-time"
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+if (Storage.load(LOCAL_STORAGE_KEY)) {
+    const { time } = Storage.load(LOCAL_STORAGE_KEY);
+
+    player.setCurrentTime(time).then(function (seconds) {}).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+
+            default:
+                break;
+        }
+    });
+}
+
+const onPlay = function (data) {
+    const currentTime = {};
+    currentTime.time = data.seconds;
+
+    Storage.save(LOCAL_STORAGE_KEY, currentTime);
+};
+
+player.on('timeupdate', onPlay);
 
